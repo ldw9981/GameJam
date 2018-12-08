13ea4664759c03cae60a8ef90d7a351ded263aa4
DESKTOP-59A58HT\ldw9981 <ldw9981@gmail.com>
2018-12-09 05:00:01 +0900

    패키지 빌드 오류 수정

--- Source/GameJam/CustomComponents/CharacterStateComponent.cpp
+++ Source/GameJam/CustomComponents/CharacterStateComponent.cpp
@@ -66,7 +66,7 @@ void UCharacterStateComponent::TickComponent(float DeltaTime, ELevelTick TickTyp
 }
 
 
-inline void UCharacterStateComponent::SetCurrentState(ECharacterState NewCurrentState)
+void UCharacterStateComponent::SetCurrentState(ECharacterState NewCurrentState)
 {
 	if (NewCurrentState != CurrentState)
 	{
@@ -80,7 +80,7 @@ ECharacterState UCharacterStateComponent::GetCurrentState()
 	return CurrentState;
 }
 
-inline bool UCharacterStateComponent::IsDead()
+bool UCharacterStateComponent::IsDead()
 {
 	if (CurrentState == ECharacterState::Dead)
 		return true;
